d0b830d046500439621537eae38d1f63ff0a1f2e
Mykola Saliuk <haker0502@ukr.net>
2023-01-29 18:04:53 +0200

    переносить Modal на хуки

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,41 +1,89 @@
 import { PropTypes } from 'prop-types';
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import { ModalImg, Overlay } from './Modal.styled';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export class Modal extends Component {
-  static propTypes = {
-    onClose: PropTypes.func.isRequired,
-    children: PropTypes.node,
-  };
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
+export const Modal = ({ onClose, children }) => {
+  useEffect(() => {
+    const handleKeyDown = evt => {
+      console.log(evt.code);
+      if (evt.code === 'Escape') {
+        onClose();
+      }
+    };
+    window.addEventListener('keydown', handleKeyDown);
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [onClose]);
 
-  handleKeyDown = evt => {
-    if (evt.code === 'Escape') {
-      this.props.onClose();
-    }
-  };
-
-  handleBackDropClick = evt => {
+  const handleBackDropClick = evt => {
     if (evt.target === evt.currentTarget) {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  render() {
-    return createPortal(
-      <Overlay onClick={this.handleBackDropClick}>
-        <ModalImg>{this.props.children}</ModalImg>
-      </Overlay>,
-      modalRoot
-    );
-  }
-}
+  return createPortal(
+    <Overlay onClick={handleBackDropClick}>
+      <ModalImg>{children}</ModalImg>
+    </Overlay>,
+    modalRoot
+  );
+};
+
+Modal.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  children: PropTypes.node,
+};
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+// import { PropTypes } from 'prop-types';
+// import { Component } from 'react';
+// import { createPortal } from 'react-dom';
+// import { ModalImg, Overlay } from './Modal.styled';
+
+// const modalRoot = document.querySelector('#modal-root');
+
+// export class Modal extends Component {
+//   static propTypes = {
+//     onClose: PropTypes.func.isRequired,
+//     children: PropTypes.node,
+//   };
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   handleKeyDown = evt => {
+//     if (evt.code === 'Escape') {
+//       this.props.onClose();
+//     }
+//   };
+
+//   handleBackDropClick = evt => {
+//     if (evt.target === evt.currentTarget) {
+//       this.props.onClose();
+//     }
+//   };
+
+//   render() {
+//     return createPortal(
+//       <Overlay onClick={this.handleBackDropClick}>
+//         <ModalImg>{this.props.children}</ModalImg>
+//       </Overlay>,
+//       modalRoot
+//     );
+//   }
+// }
