72952f8664c280a6684c4271fd80deffa8e3d81f
Mykola Saliuk <haker0502@ukr.net>
2023-01-29 18:01:40 +0200

    переносить код Searchbar на хуки

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,5 +1,5 @@
 import { PropTypes } from 'prop-types';
-import { Component } from 'react';
+import { useState } from 'react';
 import { toast } from 'react-toastify';
 import { BiSearchAlt } from 'react-icons/bi';
 
@@ -11,53 +11,127 @@ import {
   SearchInput,
 } from './Searchbar.styled';
 
-export class Searchbar extends Component {
-  static propTypes = {
-    onSubmit: PropTypes.func.isRequired,
+export const Searchbar = ({ onSubmit }) => {
+  const [searchInput, setSearchInput] = useState('');
+
+  const handleInputChange = evt => {
+    setSearchInput(evt.currentTarget.value.toLowerCase());
   };
 
-  state = {
-    searchInput: '',
-  };
-
-  handleInputChange = evt => {
-    this.setState({ searchInput: evt.currentTarget.value.toLowerCase() });
-  };
-
-  handleSubmit = evt => {
+  const handleSubmit = evt => {
     evt.preventDefault();
 
-    if (this.state.searchInput.trim()) {
-      this.props.onSubmit(this.state.searchInput);
-      this.setState({ searchInput: '' });
+    if (searchInput.trim()) {
+      onSubmit(searchInput);
+      setSearchInput('');
       return;
     }
     toast.error('Please enter the text in the search field! 🔍', {});
   };
 
-  render() {
-    const { searchInput } = this.state;
+  return (
+    <SearchbarHeader>
+      <SearchForm onSubmit={handleSubmit}>
+        <SearchFormBtn type="submit">
+          <BiSearchAlt />
+          <ButtonLabel>Search</ButtonLabel>
+        </SearchFormBtn>
 
-    return (
-      <SearchbarHeader>
-        <SearchForm onSubmit={this.handleSubmit}>
-          <SearchFormBtn type="submit">
-            <BiSearchAlt />
-            <ButtonLabel>Search</ButtonLabel>
-          </SearchFormBtn>
+        <SearchInput
+          className="input"
+          type="text"
+          name="searchInput"
+          value={searchInput}
+          onChange={handleInputChange}
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+      </SearchForm>
+    </SearchbarHeader>
+  );
+};
 
-          <SearchInput
-            className="input"
-            type="text"
-            name="searchInput"
-            value={searchInput}
-            onChange={this.handleInputChange}
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-          />
-        </SearchForm>
-      </SearchbarHeader>
-    );
-  }
-}
+Searchbar.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
+
+// export class Searchbar extends Component {
+
+//   render() {
+//     const { searchInput } = this.state;
+
+//   }
+// }
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+// import { PropTypes } from 'prop-types';
+// import { Component } from 'react';
+// import { toast } from 'react-toastify';
+// import { BiSearchAlt } from 'react-icons/bi';
+
+// import {
+//   ButtonLabel,
+//   SearchbarHeader,
+//   SearchForm,
+//   SearchFormBtn,
+//   SearchInput,
+// } from './Searchbar.styled';
+
+// export class Searchbar extends Component {
+//   static propTypes = {
+//     onSubmit: PropTypes.func.isRequired,
+//   };
+
+//   state = {
+//     searchInput: '',
+//   };
+
+//   handleInputChange = evt => {
+//     this.setState({ searchInput: evt.currentTarget.value.toLowerCase() });
+//   };
+
+//   handleSubmit = evt => {
+//     evt.preventDefault();
+
+//     if (this.state.searchInput.trim()) {
+//       this.props.onSubmit(this.state.searchInput);
+//       this.setState({ searchInput: '' });
+//       return;
+//     }
+//     toast.error('Please enter the text in the search field! 🔍', {});
+//   };
+
+//   render() {
+//     const { searchInput } = this.state;
+
+//     return (
+//       <SearchbarHeader>
+//         <SearchForm onSubmit={this.handleSubmit}>
+//           <SearchFormBtn type="submit">
+//             <BiSearchAlt />
+//             <ButtonLabel>Search</ButtonLabel>
+//           </SearchFormBtn>
+
+//           <SearchInput
+//             className="input"
+//             type="text"
+//             name="searchInput"
+//             value={searchInput}
+//             onChange={this.handleInputChange}
+//             autoComplete="off"
+//             autoFocus
+//             placeholder="Search images and photos"
+//           />
+//         </SearchForm>
+//       </SearchbarHeader>
+//     );
+//   }
+// }
